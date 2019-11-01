9a49eb08f17255151e104ab6e2f47e6f8506dfdb
Joe Klause <joeklause@gmail.com>
2019-11-01 12:30:31 -0700

    done hashTable

---
+++ challenges/hash-table/__test__/hashTable.test.js
@@ -0,0 +1,40 @@
+const HashTable = require('../hashTable');
+
+describe('HashTable', () => {
+  it('instantiates an array of empty arrays of a specified length', ()=> {
+    const hashTable = new HashTable();
+    expect(hashTable.arr.length).toBe(20);
+    expect(hashTable.arr[0]).toEqual([]);
+
+  });
+  it('hashes a key', () => {
+    const hashTable = new HashTable();
+    expect(hashTable.hash('Joe')).toBe(9);
+  });
+  it('sets a value from a key in array', ()=> {
+    const hashTable = new HashTable();
+    const key = 'Joe';
+    const value = 'Klause';
+    const referenceIndex = hashTable.hash(key);
+    hashTable.set(key, value);
+    expect(hashTable.arr[referenceIndex][0]).toStrictEqual([key, value]);
+  });
+  it('gets a value from a key in array', ()=> {
+    const hashTable = new HashTable();
+    const key = 'Joe';
+    const value = 'Klause';
+    hashTable.set(key, value);
+    expect(hashTable.get(key)).toBe(value);
+  });
+  
+  it('will update value in an array', ()=> {
+    const hashTable = new HashTable();
+    const key = 'Joe';
+    const value = 'Klause';
+    const newValue = 'LaCavera';
+    const referenceIndex = hashTable.hash(key);
+    hashTable.set(key, value);
+    hashTable.set(key, newValue);
+    expect(hashTable.arr[referenceIndex][0]).toStrictEqual([key, newValue]);
+  });
+});
